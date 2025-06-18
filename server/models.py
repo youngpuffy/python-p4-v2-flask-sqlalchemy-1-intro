0b5049f094eeff65ec698e3f9eb87c4c38dc036c
Youngpuffy <levimuturi2@gmail.com>
2025-06-18 13:13:40 +0300

    finished

--- server/models.py
+++ server/models.py
@@ -16,3 +16,12 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
